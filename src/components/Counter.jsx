b627d517fbd7ee4e94a32cbb2ba614304a048f61
aka <aaronkaltman@gmail.com>
2023-12-13 18:05:19 -0500

    counter header logo

---
+++ src/components/Counter.jsx
@@ -0,0 +1,3 @@
+export default function Counter() {
+  return <p>1/3 items packed</p>;
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import Logo from "./Logo";
+import Counter from "./Counter";
+
 export default function Header() {
   return;
-  <header>
-    <Logo />
-    <Counter />
-  </header>;
+  <>
+    <header>
+      <Logo />
+      <Counter />
+    </header>
+  </>;
 }
