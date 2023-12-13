2d0dfa4d48c7c2c1d9f5c05247bdbd572b211bb6
aka <aaronkaltman@gmail.com>
2023-12-13 17:50:21 -0500

    addinb basics - footer header etc..

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import BackgroundHeading from "./BackgroundHeading";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <BackgroundHeading />
+      <main>
+        <h1>Counter</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2024 Aaron K. Altman</small>
+      <p>
+        Version <b>3.1415926535</b>
+      </p>
+    </footer>
+  );
+}
